23d61beccaa8c2c611b65d2d93b9d0f50d346d10
Michel Calheiros <michel@calheiros.dev>
2022-03-14 21:40:14 -0300

    commit inicial - operadores soma e subtração

---
+++ main.py
@@ -0,0 +1,17 @@
+def main():
+  x = int(input('escolha o primeiro numero: '))
+  y = int(input('escolha o segundo numero: '))
+
+  operador = input('escolha o operador (+ - * /): ')
+
+  resultado = 0
+
+  if operador == '+':
+    resultado = x + y
+  elif operador == '-':
+    resultado = x - y
+
+  print('resultado: ', resultado)
+
+if __name__ == '__main__':
+  main()
